237de00b9fb32c9cf109468625a5ab361eb2fd10
David Cumps <david@cumps.be>
2016-04-20 20:57:09 +0200

    output some stuff for windows test

--- test.cmd
+++ test.cmd
@@ -1,6 +1,15 @@
+@ECHO OFF
+
+echo Starting Server
 start /b build\kestrel-classic.exe > output.log 2> errorOutput.log
 timeout 2
 taskkill /IM kestrel-classic.exe /F
+
+echo Output:
+type output.log
+
+echo Error Output:
+type errorOutput.log
 
 for /f %%i in ("errorOutput.log") do set size=%%~zi
 if %size% gtr 0 exit /B 1
